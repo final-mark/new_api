e62de9efe6452b3ab22af9077e8e6b205fc31f7e
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2019-05-03 20:13:40 -0300

    initial

---
+++ api_controllers/__init__.py
@@ -0,0 +1,23 @@
+from flask_restful import Resource
+from flask import request
+from requests import post
+
+
+class RefreshController(Resource):
+
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        if not username or not password:
+            return {"Username or password were not provided"}, 400
+
+        request_data = {
+            "plugin": "finalmark",
+            "action": "refresh_user",
+            "username": username,
+            "password": password
+        }
+        response = post('http://unamed_api', json=request_data)
+        return response.json(), response.status_code
